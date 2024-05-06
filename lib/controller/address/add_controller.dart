ad087450ecddf6836f3fb046532c762f7f654e06
kiwiProject <amjad88.ir@gmail.com>
2024-05-06 21:29:03 +0300

    initial commit

--- lib/controller/address/add_controller.dart
+++ lib/controller/address/add_controller.dart
@@ -1,18 +1,23 @@
 import 'dart:async';
+import 'package:geolocator/geolocator.dart';
 import 'package:get/get.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+import '../../core/classes/statusRequest.dart';
 
 abstract class AddAddressController extends GetxController {
 
 }
 
 class AddAddressControllerImp extends AddAddressController {
+
+  StatusRequest statusRequest = StatusRequest.loading;
+
   Completer<GoogleMapController>? mapController ;
 
-  CameraPosition kGooglePlex = const CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
-    zoom: 14.4746,
-  );
+  Position? position;
+
+  CameraPosition? kGooglePlex ;
 
   static const CameraPosition _kLake = CameraPosition(
       bearing: 192.8334901395799,
@@ -20,8 +25,19 @@ class AddAddressControllerImp extends AddAddressController {
       tilt: 59.440717697143555,
       zoom: 19.151926040649414);
 
+  getCurrentLocation() async {
+    position = await Geolocator.getCurrentPosition();
+    kGooglePlex = CameraPosition(
+      target: LatLng(position!.latitude, position!.longitude),
+      zoom: 14.4746,
+    );
+    statusRequest = StatusRequest.none;
+    update();
+  }
+
 @override
   void onInit() {
+  getCurrentLocation();
   mapController = Completer<GoogleMapController>();
     super.onInit();
   }
--- lib/view/address/add.dart
+++ lib/view/address/add.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:kiwi/core/classes/handlingDataView.dart';
 import '../../controller/address/add_controller.dart';
 
 class AddressAdd extends StatelessWidget {
@@ -20,27 +21,30 @@ class AddressAdd extends StatelessWidget {
       floatingActionButton: FloatingActionButton(
         elevation: 0,
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
-        onPressed: () {
-
-        },
+        onPressed: () {},
         child: const Icon(
           Icons.add,
           color: Colors.white,
         ),
       ),
       body: Container(
-        child: Column(
-          children: [
-            Expanded(
-              child: GoogleMap(
-                mapType: MapType.hybrid,
-                initialCameraPosition: controllerImp.kGooglePlex,
-                onMapCreated: (GoogleMapController mapController) {
-                  controllerImp.mapController!.complete(mapController);
-                },
-              ),
+        child: GetBuilder<AddAddressControllerImp>(
+          builder: (controllerImp) => HandlingDataView(
+            statusRequest: controllerImp.statusRequest,
+            widget: Column(
+              children: [
+                Expanded(
+                  child: GoogleMap(
+                    mapType: MapType.normal,
+                    initialCameraPosition: controllerImp.kGooglePlex!,
+                    onMapCreated: (GoogleMapController mapController) {
+                      controllerImp.mapController!.complete(mapController);
+                    },
+                  ),
+                ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
